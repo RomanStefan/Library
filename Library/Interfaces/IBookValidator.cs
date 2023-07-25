1edea57c0eed370d9f44f82c58ff0c0dff2fc3c6
DESKTOP-I7RVKI7\Stefan <Stefan Roman>
2023-07-25 11:52:31 +0300

    Implemented the BookValidator functionality and added the interface for it.

---
+++ Library/Interfaces/IBookValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Interfaces
+{
+    public interface IBookValidator
+    {
+        ValidationResult ValidateBook(Book book);
+    }
+}
--- Library/Validator/BookValidator.cs
+++ Library/Validator/BookValidator.cs
@@ -3,18 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FluentValidation;
+using Library.Interfaces;
 using Library.Models;
+using FluentValidation;
+using FluentValidation.Results;
+
 
 namespace Library.Validator
 {
-    public class BookValidator :AbstractValidator<Book>
+    public class BookValidator : AbstractValidator<Book>, IBookValidator
     {
         public BookValidator()
         {
             RuleFor(book => book.BookTitle).NotNull().WithMessage("BookTitle cannot be empty");
             RuleFor(book => book.ISBN).NotNull().WithMessage("ISBN cannot be empty");
             RuleFor(book => book.RentPrice).NotNull().WithMessage("RentPrice cannot be empty");
+        }
+
+        public ValidationResult ValidateBook(Book book)
+        {
+            var failures = new List<ValidationFailure>();
+
+            var result = Validate(book);
+
+            if (result.IsValid == false)
+            {
+                var validationMessage = BuildMessageForFailedBook(book, result);
+                var failure = new ValidationFailure(book.BookTitle, validationMessage);
+
+                failures.Add(failure);
+            }
+
+            return new ValidationResult(failures);
+        }
+
+        private string BuildMessageForFailedBook(Book book, ValidationResult result)
+        {
+            var message = new StringBuilder("");
+            var resultErrorMessages = result.Errors.Select(error => error.ErrorMessage).ToList();
+
+            message.Append($"The following issues were found in book {book.BookTitle}: ");
+            message.Append(string.Join(" ", resultErrorMessages));
+
+            return message.ToString();
         }
     }
 }
